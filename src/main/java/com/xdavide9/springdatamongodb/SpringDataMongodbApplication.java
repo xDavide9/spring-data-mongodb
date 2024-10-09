06e4ce4128baabc26ad36bfa96b20fc9d3a8e970
xdavide9 <biancodavidetech@gmail.com>
2024-10-09 12:22:46 +0200

    bootstrap project

---
+++ src/main/java/com/xdavide9/springdatamongodb/SpringDataMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.xdavide9.springdatamongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataMongodbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xdavide9/springdatamongodb/SpringDataMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xdavide9.springdatamongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataMongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
